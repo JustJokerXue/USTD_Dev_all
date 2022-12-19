7c5626e4bff92f9086a22ea896e0cad9d5187225
WangJC <1766872402@qq.com>
2022-12-19 18:58:58 +0800

    数据库

--- USTD_Dev_all/USTD_App1/admin.py
+++ USTD_Dev_all/USTD_App1/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from USTD_App1.models import Student
 from USTD_App1.models import Score
+from USTD_App1.models import Knowledge
+from USTD_App1.models import Innovation
+from USTD_App1.models import majorTechnology
+from USTD_App1.models import manage
+from USTD_App1.models import ComprehensiveDevelopment
+from USTD_App1.models import responsible
+from USTD_App1.models import administrator
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Score)
+admin.site.register(Knowledge)
+admin.site.register(Innovation)
+admin.site.register(majorTechnology)
+admin.site.register(manage)
+admin.site.register(ComprehensiveDevelopment)
+admin.site.register(responsible)
+admin.site.register(administrator)
+
+
--- USTD_Dev_all/USTD_App1/models.py
+++ USTD_Dev_all/USTD_App1/models.py
@@ -17,3 +17,53 @@ class Score(models.Model):
     zs = models.IntegerField(default=0, verbose_name='知识学习能力', null=True)
     gl = models.IntegerField(default=0, verbose_name='管理实践能力', null=True)
     zh = models.IntegerField(default=0, verbose_name='综合发展能力', null=True)
+from django.db import models
+
+
+# Create your models here.
+
+
+
+
+class Knowledge(models.Model):
+    name = models.CharField(max_length=200, verbose_name='姓名', null=True)
+    sno = models.IntegerField(default=0, verbose_name='学号', primary_key=True)
+    java = models.IntegerField(default=0, verbose_name='java课程', null=True)
+    dataStructure = models.IntegerField(default=0, verbose_name='数据结构', null=True)
+    Gaverage = models.IntegerField(default=0, verbose_name='平均绩点', null=True)
+
+
+class Innovation(models.Model):
+    name = models.CharField(max_length=200, verbose_name='姓名', null=True)
+    sno = models.IntegerField(default=0, verbose_name='学号', primary_key=True)
+    ContestRating = models.IntegerField(default=0, verbose_name='竞赛评分', null=False)
+    PatentRcoring = models.IntegerField(default=0, verbose_name='专利评分', null=False)
+    EntrepreneurialAchievement = models.IntegerField(default=0, verbose_name='创业成果评分', null=False)
+class majorTechnology(models.Model):
+    name = models.CharField(max_length=200, verbose_name='姓名', null=True)
+    sno = models.IntegerField(default=0, verbose_name='学号', primary_key=True)
+    ProjectPractice = models.IntegerField(default=0, verbose_name='项目实践', null=True)
+    PaperGrading = models.IntegerField(default=0, verbose_name='论文评分', null=False)
+    StudentTutor = models.IntegerField(default=0, verbose_name='学生导师评分', null=True)
+class manage(models.Model):
+    name = models.CharField(max_length=200, verbose_name='姓名', null=True)
+    sno = models.IntegerField(default=0, verbose_name='学号', primary_key=True)
+    community = models.IntegerField(default=0, verbose_name='社团工作评分', null=False)
+    StudentWork = models.IntegerField(default=0, verbose_name='学生工作评分', null=False)
+    ProjectTeam = models.IntegerField(default=0, verbose_name='项目团队评分', null=True)
+
+class ComprehensiveDevelopment(models.Model):
+    name = models.CharField(max_length=200, verbose_name='姓名', null=True)
+    sno = models.IntegerField(default=0, verbose_name='学号', primary_key=True)
+    physical = models.IntegerField(default=0, verbose_name='体育评分', null=True)
+    Volunteer = models.IntegerField(default=0, verbose_name='志愿活动评分', null=False)
+    Labor = models.IntegerField(default=0, verbose_name='劳务活动评分', null=False)
+    morality = models.IntegerField(default=0, verbose_name='思想道德评分', null=True)
+class responsible(models.Model):
+    name = models.CharField(max_length=200, verbose_name='姓名', null=True)
+    Employeeno = models.IntegerField(default=0, verbose_name='职工号', primary_key=True)
+    password = models.IntegerField(default=0, verbose_name='密码', null=True)
+class administrator(models.Model):
+    name = models.CharField(max_length=200, verbose_name='姓名', null=True)
+    Employeeno = models.IntegerField(default=0, verbose_name='职工号', primary_key=True)
+    password = models.IntegerField(default=0, verbose_name='密码', null=True)
